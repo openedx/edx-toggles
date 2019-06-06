6f0b0fde1f01d8b443f4c7f7f7e2ac037af2c346
Stuart Young <syoung@edx.org>
2019-06-06 10:24:48 -0400

    test cleanup

--- scripts/tests/test_feature_toggle_report_generator.py
+++ scripts/tests/test_feature_toggle_report_generator.py
@@ -4,18 +4,6 @@ from ..feature_toggle_report_generator import (
 )
 
 
-def test_waffle_switch_state():
-    switch = ToggleState(
-        'my_switch', 'waffle.switch',
-        {
-            'active': False,
-            'created': '2019-04-23 14:21:44.765727+00:00',
-            'modified': '2019-04-23 14:21:44.765738+00:00'
-        }
-    )
-    assert not switch.state
-
-
 def test_waffle_flag_state():
     null_percent_flag = ToggleState(
         'my_flag', 'waffle.flag',
@@ -57,7 +45,7 @@ def test_waffle_flag_state():
     assert language_flag.state
 
 
-def test_template_date():
+def test_template_data():
     null_percent_flag = ToggleState(
         'my_flag', 'waffle.flag',
         {
@@ -76,3 +64,7 @@ def test_template_date():
             'users': []
         }
     )
+    toggle_data = null_percent_flag.data_for_template
+    assert toggle_data['percent'] == 0
+    assert toggle_data['languages'] == ['en-US', 'es-ES']
+    assert toggle['created'] == '2019-04-23 14:21'
