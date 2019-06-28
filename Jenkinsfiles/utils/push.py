6ed1b8a50cd988d9cad81559a916de6fcf7b7633
Stuart Young <syoung@edx.org>
2019-06-28 14:28:45 -0400

    fixup

--- Jenkinsfiles/utils/push.py
+++ Jenkinsfiles/utils/push.py
@@ -8,15 +8,22 @@ import boto3
 
 
 def main():
+
     # make sure path to bucket is available
     s3_connection = boto.connect_s3()
     bucket = s3_connection.get_bucket('your bucket name')
     key = boto.s3.key.Key(bucket, 'some_file.zip')
 
+    annotation_files = [
+        os.path.join(path, af)
+        for af in os.listdir(path)
+        if af.ends_with('yaml')
+    ]
 
-    for annotation_file in 'path':
-        with open('some_file.zip') as f:
-                key.send_file(f)
+    for annotation_file in annotation_files:
+        with open(annotation_file) as af:
+            key.send_file(af)
+
 
 if __name__ == "__main__":
     main()
