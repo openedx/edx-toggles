b1a7e62132271d408c5e0b92f6140741f98d9e1d
Stuart Young <syoung@edx.org>
2019-06-28 14:45:59 -0400

    fixup

--- Jenkinsfiles/utils/push.py
+++ Jenkinsfiles/utils/push.py
@@ -1,18 +1,23 @@
+from __future__ import print_function
+
 import os
-import boto3
+import sys
 
-
-
-
-
+import boto
 
 
 def main():
 
-    # make sure path to bucket is available
-    s3_connection = boto.connect_s3()
-    bucket = s3_connection.get_bucket('your bucket name')
-    key = boto.s3.key.Key(bucket, 'some_file.zip')
+    try:
+        conn = boto.connect_s3()
+    except boto.exception.NoAuthHandlerFound:
+        print("No AWS credentials found.")
+        sys.exit(1)
+    try:
+        bucket = conn.get_bucket(bucket_name)
+    except boto.exception.S3ResponseError:
+        print("Unable to connect to cache bucket with these credentials.")
+        sys.exit(1)
 
     annotation_files = [
         os.path.join(path, af)
@@ -22,8 +27,12 @@ def main():
 
     for annotation_file in annotation_files:
         with open(annotation_file) as af:
-            key.send_file(af)
-
+            key = boto.s3.key.Key(bucket=bucket, name=file_name)
+            bytes_written = key.set_contents_from_filename(
+                af, replace=True, policy='private'
+            )
+            if bytes_written:
+                print(u"Wrote {} bytes to {}.".format(bytes_written, key.name))
 
 if __name__ == "__main__":
     main()
