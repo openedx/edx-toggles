945c76d0c318ddd7f34b77cb458b510afe105947
Stuart Young <syoung@edx.org>
2019-07-02 11:19:26 -0400

    cleanup

--- scripts/publish-to-s3.py
+++ scripts/publish-to-s3.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env
 
+# publish-to-s3.py
+#
+# push all of the files in 'annotation_reports' to an S3 bucket
+# specified with the environment variable 'ANNOTATION_REPORT_BUCKET'
+
+from __future__ import absolute_import
+from __future__ import print_function
 import os
 import sys
 
@@ -8,36 +15,38 @@ import boto
 
 def main():
 
-    bucket_name = os.environ.get('BUCKET')
+    bucket_name = os.environ.get('ANNOTATION_REPORT_BUCKET')
     if not bucket_name:
-        print "Missing environment variable"
+        print("Missing environment variable 'ANNOTATION_REPORT_BUCKET'")
         sys.exit(1)
 
     try:
         conn = boto.connect_s3()
     except boto.exception.NoAuthHandlerFound:
-        print "No AWS credentials found."
+        print("No AWS credentials/roles found on this server")
         sys.exit(1)
     try:
         bucket = conn.get_bucket(bucket_name)
     except boto.exception.S3ResponseError:
-        print "Unable to connect to cache bucket with these credentials."
+        print("Unable to connect to bucket with these credentials.")
         sys.exit(1)
 
     path = 'annotation_reports'
     annotation_files = [
-        (os.path.join(path, af), "annotation_reports/{}".format(af))
+        (os.path.join(path, af), "{}/{}".format(path, af))
         for af in os.listdir(path)
         if af.endswith('yaml') or af.endswith('yml')
     ]
 
+    print("Writing data from {} to s3 bucket".format(path))
     for annotation_file_src, annotation_file_dest in annotation_files:
         key = boto.s3.key.Key(bucket=bucket, name=annotation_file_dest)
         bytes_written = key.set_contents_from_filename(
             annotation_file_dest, replace=True, policy='private'
         )
         if bytes_written:
-            print u"Wrote {} bytes to {}.".format(bytes_written, key.name)
+            print(u"Wrote {} bytes to {}.".format(bytes_written, key.name))
+
 
 if __name__ == "__main__":
     main()
