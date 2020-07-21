87d09513c1ef1ff9033eef055fde36dcb7f1f16e
jinder1s <msingh@edx.org>
2020-07-21 15:41:20 -0400

    Removing unnecessary prints

--- scripts/feature_toggle_report_generator.py
+++ scripts/feature_toggle_report_generator.py
@@ -106,9 +106,7 @@ def main(annotations_dir, toggle_data_dir, output_file_path, show_state, env, to
         # add data for each ida
         if show_state:
             add_toggle_state_to_idas(total_info[env_name], env_data_path, configuration.get("ida", defaultdict(dict)))
-            print("INFO 1 : idas in {}: {}".format(env_name, len(total_info[env_name])))
         add_toggle_annotations_to_idas(total_info[env_name], annotations_dir, configuration.get("ida", defaultdict(dict)))
-        print("INFO 2: idas in {}: {}".format(env_name, len(total_info[env_name])))
 
     renderer = CsvRenderer()
     renderer.render_csv_report(total_info, output_file_path, toggle_type_filter)
