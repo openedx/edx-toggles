7ea13838e5fdece74a5168350932c6e1594535fc
jinder1s <msingh@edx.org>
2020-09-21 14:36:56 -0400

    Correcting toggle type input

--- scripts/feature_toggle_report_generator.py
+++ scripts/feature_toggle_report_generator.py
@@ -74,7 +74,7 @@ def main(annotations_dir, toggle_data_dir, output_file_path, show_state, env, to
     # commandline-option inputs overwrite stuff in configuration file
     toggle_type_filter = toggle_type
     if not toggle_type and "toggle_type" in configuration.keys():
-        toggle_type_filter = [configuration["toggle_type"]
+        toggle_type_filter = [ToggleTypes.get_internally_consistent_toggle_type(toggle_type) for toggle_type in configuration["toggle_type"]]
 
     requested_envs = env
     if not env and "env" in configuration.keys():
