9e0c83805034d59a2cbf920bfba3c9119444d7b2
Régis Behmo <regis@behmo.com>
2020-11-03 11:31:07 +0100

    Move soon-to-be-deprecated waffle code to legacy module
    This will be helpful in viewing the differences between the modules.

--- edx_toggles/toggles/__init__.py
+++ edx_toggles/toggles/__init__.py
@@ -2,4 +2,4 @@
 Expose public feature toggle API.
 """
 from .internal.setting_toggle import SettingDictToggle, SettingToggle
-from .internal.waffle import WaffleFlag, WaffleFlagNamespace, WaffleSwitch, WaffleSwitchNamespace
+from .internal.waffle.legacy import WaffleFlag, WaffleFlagNamespace, WaffleSwitch, WaffleSwitchNamespace
