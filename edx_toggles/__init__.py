bf40eea61aa2ef9a596f691eade93203010602a3
Jawayria <39649635+Jawayria@users.noreply.github.com>
2021-07-12 13:42:31 +0500

    feat: Added support for django3.2 (#150)

--- edx_toggles/__init__.py
+++ edx_toggles/__init__.py
@@ -2,6 +2,6 @@
 Library and utilities for feature toggles.
 """
 
-__version__ = '4.1.0'
+__version__ = '4.2.0'
 
 default_app_config = 'edx_toggles.apps.TogglesConfig'  # pylint: disable=invalid-name
