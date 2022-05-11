fedeafb072e104318b98157417004f67b6b32b24
Eugene Dyudyunov <evgen.dyudyunov@raccoongang.com>
2022-05-11 22:02:02 +0300

    feat!: remove legacy waffle flags (#211)
    This is a part of https://github.com/openedx/public-engineering/issues/28
    
    BREAKING CHANGE: Removed LegacyWaffle* classes. Although this is
    a breaking change, all known uses have already been fixed.

--- edx_toggles/toggles/__init__.py
+++ edx_toggles/toggles/__init__.py
@@ -3,10 +3,4 @@ Expose public feature toggle API.
 """
 from .internal.setting_toggle import SettingDictToggle, SettingToggle
 from .internal.waffle.flag import NonNamespacedWaffleFlag, WaffleFlag
-from .internal.waffle.legacy import (
-    LegacyWaffleFlag,
-    LegacyWaffleFlagNamespace,
-    LegacyWaffleSwitch,
-    LegacyWaffleSwitchNamespace
-)
 from .internal.waffle.switch import NonNamespacedWaffleSwitch, WaffleSwitch
