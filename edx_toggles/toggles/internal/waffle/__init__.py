60e4635d9281533ff99f3cd35e8c675e5328f61d
Sarina Canelake <sarina@tcril.org>
2022-09-14 16:16:28 -0400

    chore: run edx_lint update with the current version of the repo.

--- edx_toggles/toggles/internal/waffle/__init__.py
+++ edx_toggles/toggles/internal/waffle/__init__.py
@@ -1,5 +1,6 @@
 """
 Waffle classes in the context of edx-platform and other IDAs.
 
-For detailed usage see https://github.com/edx/edx-toggles/blob/master/docs/how_to/implement_the_right_toggle_type.rst
+For detailed usage see:
+  https://github.com/openedx/edx-toggles/blob/master/docs/how_to/implement_the_right_toggle_type.rst
 """
